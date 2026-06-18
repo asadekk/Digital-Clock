5b4e5b7471cf732400988bcd6b5e8695e326d8ac
asadekk <asadesd21@gmail.com>
2026-06-18 17:14:23 +0500

    Fix: Add .jsx extensions to imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 // ===
-import Clock from './pages/Clock'
+import Clock from './pages/Clock.jsx'       
+import Layout from './layout/Layout.jsx' 
 import Timer from './pages/Timer'
 import Alarm from './pages/Alarm'
-import Layout from './layout/Layout'
 import Worldclock from './pages/Worldclock';
 // ===
 
